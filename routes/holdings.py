f423a2625ec0603f0244885f28203a79ea39fdcb
Sam <psyos12@nottingham.ac.uk>
2026-09-20 15:20:11 +0100

    Add update and delete holdings endpoints

--- routes/holdings.py
+++ routes/holdings.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from schemas.holding import HoldingCreate, Holding
 from services import holding_service
 
@@ -11,3 +11,17 @@ def create_holding(holding: HoldingCreate):
 @router.get("")
 def get_holdings():
     return holding_service.list_all_holdings()
+
+@router.delete("/{holding_id}", status_code=204)
+def delete_holding(holding_id: int):
+    deleted = holding_service.delete_holding(holding_id)
+    if not deleted:
+        raise HTTPException(status_code=404, detail="Holding not found")
+    
+@router.put("/{holding_id}")
+def update_holding(holding_id: int, new_data: HoldingCreate):
+    updated = holding_service.update_holding(holding_id, new_data)
+    if not updated:
+        raise HTTPException(status_code=404, detail="Holding not found")
+    return updated
+
--- services/holding_service.py
+++ services/holding_service.py
@@ -6,6 +6,19 @@ def list_all_holdings():
 
 def add_holding(holding: HoldingCreate):
     new = Holding(id=len(holdings) + 1, **holding.model_dump())
-    holding.append(new)
+    holdings.append(new)
     return new
 
+def delete_holding(holding_id: int):
+    for holding in holdings:
+        if holding.id == holding_id:
+            holdings.remove(holding)
+            return True
+    return False
+    
+def update_holding(holding_id: int, new_data: HoldingCreate):
+    for i, holding in enumerate(holdings):
+        if holding.id == holding_id:
+            holdings[i] = Holding(id=holding_id, **new_data.model_dump())
+            return holdings[i]
+    return None
